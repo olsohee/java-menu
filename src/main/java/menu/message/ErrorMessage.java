e664c173eca50044e9767513faef4a45cb2fe6fa
olsohee <dlthgml0108@daum.net>
2023-12-15 14:48:53 +0900

    style: 불필요한 코드 제거

--- src/main/java/menu/message/ErrorMessage.java
+++ src/main/java/menu/message/ErrorMessage.java
@@ -2,7 +2,6 @@ package menu.message;
 
 public enum ErrorMessage {
 
-    INVALID_INPUT("잘못된 입력입니다."),
     INVALID_COACH_NAME_LENGTH("이름은 최소 2글자에서 최대 4글자여야 합니다."),
     DUPLICATED_COACH_NAME("이름은 중복될 수 없습니다."),
     INVALID_COACH_COUNT("코치는 최소 2명에서 최대 5명이어야 합니다."),
--- src/main/java/menu/message/OutputMessage.java
+++ src/main/java/menu/message/OutputMessage.java
@@ -8,8 +8,6 @@ public enum OutputMessage {
     RESULT_FORM("[ %s ]"),
     DIVISION("구분"),
     CATEGORY("카테고리"),
-    DAY_OF_WEEK("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"),
-
     END_MESSAGE("추천을 완료했습니다."),
     ;
 
